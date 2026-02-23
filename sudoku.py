d925770d982c20098b832a70211fece083a6f4be
Kaviya-106 <147529355+Kaviya-106@users.noreply.github.com>
2026-02-23 16:27:28 +0100

    Update sudoku.py
    vérifie si c'est valide et mélange

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,21 @@
 import pygame
 import random
- 
+
+
+def is_valid(board, idx, val):
+    row, col = divmod(idx, 9)
+    br, bc = (row // 3) * 3, (col // 3) * 3
+    for i in range(9):
+        if board[row * 9 + i] == val:
+            return False
+        if board[i * 9 + col] == val:
+            return False
+        if board[(br + i // 3) * 9 + bc + i % 3] == val:
+            return False
+    return True
+
+
+def shuffle(lst):
+    lst = list(lst)
+    random.shuffle(lst)
+    return lst
